cdaad40c760d3fce76a98a9b679dfe3b713ce829
Max Kaye <m@xk.io>
2023-05-08 12:55:40 +1000

    remove files too

--- itemrefresh/gbxnet.py
+++ itemrefresh/gbxnet.py
@@ -253,9 +253,9 @@ def run_place_objects_on_map(
     print([config_path, _out_map_path, _populated_out_map_path])
     ret_bytes = Path(_out_map_path if overwrite else _populated_out_map_path).read_bytes()
     print(ret_bytes[:10])
-    # os.remove(config_path)
-    # os.remove(_out_map_path)
-    # os.remove(_populated_out_map_path)
+    os.remove(config_path)
+    os.remove(_out_map_path)
+    os.remove(_populated_out_map_path)
     return ret_bytes
 
 
