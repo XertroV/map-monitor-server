01a0c1a06b72a668403ceda0e1ba540e029e643d
Max Kaye <m@xk.io>
2023-09-21 11:09:22 +1000

    fix migration again

--- getrecords/migrations/0035_alter_cotdchallengeranking_unique_together.py
+++ getrecords/migrations/0035_alter_cotdchallengeranking_unique_together.py
@@ -3,17 +3,24 @@
 from django.db import migrations
 from django.db.models import Min, Count
 
+from getrecords.models import CotdChallenge
+
 def delete_challenge_record_duplicates(apps, schema_editor):
     CotdChallengeRanking = apps.get_model("getrecords", "CotdChallengeRanking")
-    non_dupe_pks = list(
-        CotdChallengeRanking.objects.values('challenge', 'req_timestamp', 'rank')
-            .annotate(Min('pk'), count=Count('pk'))
-            .order_by()
-            .values_list('pk__min', flat=True)
-    )
+    nb_challenges = CotdChallenge.objects.count()
+    print(f"Checking {nb_challenges} challenges")
+    for i, challenge in enumerate(CotdChallenge.objects.all()):
+        non_dupe_pks = list(
+            CotdChallengeRanking.objects.values('challenge', 'req_timestamp', 'rank')
+                .annotate(Min('pk'), count=Count('pk'))
+                .order_by()
+                .values_list('pk__min', flat=True)
+        )
 
-    dupes = CotdChallengeRanking.objects.exclude(pk__in=non_dupe_pks)
-    dupes.delete()
+        dupes = CotdChallengeRanking.objects.exclude(pk__in=non_dupe_pks)
+        dupes.delete()
+        print(f"Done {challenge.challenge_id}; {i / nb_challenges * 100:.1f}")
+    print(f"Done all")
 
 class Migration(migrations.Migration):
 
