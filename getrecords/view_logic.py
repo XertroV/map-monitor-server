8d7bd20bb4d3f18509ec1f01f94ecd312fed9a9e
Max Kaye <m@xk.io>
2024-01-08 18:16:55 +1100

    revert inprog fix

--- getrecords/view_logic.py
+++ getrecords/view_logic.py
@@ -27,10 +27,10 @@ async def refresh_nb_players_inner(map_uid: str, updated_ago_min_secs=0) -> tupl
         in_prog = mtp.last_update_started_ts >= mtp.updated_ts # and (time.time() - mtp.last_update_started_ts < 60)
         # check for explict only-update-in-some-cases flag, used by AT/TMX scraper
         if (delta < updated_ago_min_secs):
-            return (mtp, in_prog)
+            return (mtp, False)
         # if it's been less than 5 minutes, or an update is in prog, return cached, refresh soon if in progress
         if not LOCAL_DEV_MODE and (in_prog or delta < NB_PLAYERS_CACHE_SECONDS):
-            return (mtp, in_prog)
+            return (mtp, False)
     else:
         mtp = MapTotalPlayers(uid=map_uid)
     mtp.last_update_started_ts = time.time()
