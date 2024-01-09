99314e8b582d8f807d476e044569d2ac6991aaf9
Max Kaye <m@xk.io>
2024-01-10 10:59:28 +1100

    try fix

--- getrecords/view_logic.py
+++ getrecords/view_logic.py
@@ -15,7 +15,7 @@ NB_PLAYERS_MAX_CACHE_SECONDS = 8 * 60 * 60
 # 10 sec
 QUALI_TIMES_CACHE_SECONDS = 10
 
-async def refresh_nb_players_inner(map_uid: str, updated_ago_min_secs=0) -> tuple[MapTotalPlayers, bool]:
+async def refresh_nb_players_inner(map_uid: str, updated_ago_min_secs=-1) -> tuple[MapTotalPlayers, bool]:
     ''' returns map info + refresh soon flag '''
     mtps = MapTotalPlayers.objects.filter(uid=map_uid)
     last_known = 0
@@ -24,13 +24,13 @@ async def refresh_nb_players_inner(map_uid: str, updated_ago_min_secs=0) -> tupl
         mtp = await mtps.afirst()
         last_known = mtp.nb_players
         delta = time.time() - mtp.updated_ts
-        in_prog = mtp.last_update_started_ts >= mtp.updated_ts # and (time.time() - mtp.last_update_started_ts < 60)
+        in_prog = mtp.last_update_started_ts >= mtp.updated_ts and (time.time() - mtp.last_update_started_ts < 60)
         # check for explict only-update-in-some-cases flag, used by AT/TMX scraper
         if (delta < updated_ago_min_secs):
             return (mtp, False)
         # if it's been less than 5 minutes, or an update is in prog, return cached, refresh soon if in progress
         if not LOCAL_DEV_MODE and (in_prog or delta < NB_PLAYERS_CACHE_SECONDS):
-            return (mtp, False)
+            return (mtp, in_prog)
     else:
         mtp = MapTotalPlayers(uid=map_uid)
     mtp.last_update_started_ts = time.time()
