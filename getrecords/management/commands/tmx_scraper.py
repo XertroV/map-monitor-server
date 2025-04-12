7ebe204f2652bf29af7684927ea27cf96f0d68f1
Max Kaye <m@xk.io>
2025-04-12 19:55:20 +1000

    wait for updates to tmx maps after fixing broken AT maps

--- getrecords/management/commands/tmx_scraper.py
+++ getrecords/management/commands/tmx_scraper.py
@@ -84,6 +84,7 @@ async def run_tmx_scraper(state: TmxMapScrapeState, update_state: TmxMapScrapeSt
     global first_run
     # temp fix for maps being marked as removed from tmx
     await fix_tmx_records()
+    await run_check_tmx_unbeaten_removed_updated()
 
     loop_seconds = 300
     while True:
@@ -718,7 +719,7 @@ async def get_maps_from_tmx(tids_or_uids: list[int | str]) -> list[dict]:
 async def fix_tmx_records():
     # return
 
-    # ---- 2025-04-12, most unbeaten got marked as removed.
+    # ---- 2025-04-12, most unbeaten got marked as removed from tmx; also some broken not broken.
     q = TmxMapAT.objects.filter(
         Broken=True
     ).select_related('Track')
